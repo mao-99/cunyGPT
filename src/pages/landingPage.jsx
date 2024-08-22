912b416f286f949279328684243759523c5746e0
Jack Hachicho <jackh1212@hotmail.com>
2024-08-22 13:21:49 -0400

    post-PR bug fixes

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Heading, Button, Divider, Flex, Image, Text } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-import ParticlesComponent from '/Users/harmainmunir/Desktop/test_cunyGPT/cunyGPT/src/components/particles.jsx';
+import ParticlesComponent from '../components/particles';
 import { TypeAnimation } from 'react-type-animation';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -136,119 +136,41 @@ const LandingPage = () => {
           justify="space-between"
           textAlign={{ base: 'center', md: 'left' }}
           mb={12}
-    <Box>
-      <Flex justify="center" mt={20}>
-        <Image src="placeholder.jpg" alt="Placeholder" />
-      </Flex>
-      <Flex justify="center" mt={6}>
-        <Button
-          colorScheme="blue"
-          color="#FFB71B"
-          bg="#204CAD"
-          _hover={{ bg: '#071B49' }}
-          as={RouterLink}
-          to="/messaging"
-        >
-          Try now for free
-        </Button>
-      </Flex>
-      <Divider my={12} />
-      <Flex justify="center" align="center" mb={8}>
-        <Image src="placeholder.jpg" alt="Placeholder" mr={8} />
-        <Box>
-          <Heading mb={2} fontSize="2xl">The Problem</Heading>
-          <Text>
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum
-          </Text>
-        </Box>
-      </Flex>
-      <Flex justify="center" align="center" mb={8}>
-        <Box>
-          <Heading mb={2} fontSize="2xl">Our Solution</Heading>
-          <Text>
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum
-          </Text>
-        </Box>
-        <Image src="placeholder.jpg" alt="Placeholder" ml={8} />
-      </Flex>
-      <Heading mb={4} textAlign="center">
-        See how it works
-      </Heading>
-      <Flex justify="center" mb={12}>
-        <Image src="placeholder.gif" alt="Placeholder" />
-      </Flex>
-      
-      <Heading mb={4} textAlign="center">
-        CunyGPT is connected to:
-      </Heading>
-      {/* Left to right slider */}
-      <SliderContainer>
-        <Slider {...sliderSettings}>
-          {cunyImages.map((img, index) => (
-            <SliderImage key={index} src={img} alt={`CUNY ${index + 1}`} />
-          ))}
-        </Slider>
-      </SliderContainer>
-
-      {/* Right to left slider */}
-      <SliderContainer>
-        <Slider {...{ ...sliderSettings, rtl: true }}>
-          {cunyImages.map((img, index) => (
-            <SliderImage key={index} src={img} alt={`CUNY ${cunyImages.length - index}`} />
-          ))}
-        </Slider>
-      </SliderContainer>
-      
-      <Flex justify="center">
-        <Button
-          colorScheme="blue"
-          color="#FFB71B"
-          bg="#204CAD"
-          _hover={{ bg: '#071B49' }}
-          as={RouterLink}
-          to="/messaging"
-          mb={3}
         >
           <Box flex="1" pr={{ base: 0, md: 8 }} mb={{ base: 8, md: 0 }}>
             <Heading fontSize={{ base: '3xl', md: '4xl', lg: '6xl' }} 
-            color="#FFFFFF" 
-            mb={2}
-            fontWeight="bold"
-            textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)"
-            style={{ whiteSpace: 'nowrap' }} // Prevents text wrapping
-
+              color="#FFFFFF" 
+              mb={2}
+              fontWeight="bold"
+              textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)"
+              style={{ whiteSpace: 'nowrap' }}
             >
-    
               Welcome to CUNY GPT
             </Heading>
 
             <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Your Virtual AI Assistant',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Your Hub for All CUNY Wellness Resources',
-        1000,
-        'Your Guide to Life at CUNY',
-        1000,
-        'Your ',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{
-        fontSize: '1.5em', // Increased font size
-        display: 'inline-block',
-        color: "#FFFFFF",
-        fontWeight: 'bold', // Made text bolder
-        textShadow: '3px 3px 6px rgba(0, 0, 0, 0.7)', // Enhanced text shadow
-        letterSpacing: '0.05em' // Added letter spacing
-      }}
-      repeat={Infinity}
-    />
-
+              sequence={[
+                'Your Virtual AI Assistant',
+                1000,
+                'Your Hub for All CUNY Wellness Resources',
+                1000,
+                'Your Guide to Life at CUNY',
+                1000,
+                'Your ',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{
+                fontSize: '1.5em',
+                display: 'inline-block',
+                color: "#FFFFFF",
+                fontWeight: 'bold',
+                textShadow: '3px 3px 6px rgba(0, 0, 0, 0.7)',
+                letterSpacing: '0.05em'
+              }}
+              repeat={Infinity}
+            />
            
             <Flex justify={{ base: 'center', md: 'flex-start' }} mt={8} ml={9}>
               <Button
@@ -305,9 +227,28 @@ const LandingPage = () => {
         <Flex justify="center" mb={12}>
           <Image src="placeholder.gif" alt="Placeholder" />
         </Flex>
-        <Flex justify="center" mb={12}>
-          <Text>CunyGPT is connected to:</Text>
-        </Flex>
+        
+        <Heading mb={4} textAlign="center">
+          CunyGPT is connected to:
+        </Heading>
+        {/* Left to right slider */}
+        <SliderContainer>
+          <Slider {...sliderSettings}>
+            {cunyImages.map((img, index) => (
+              <SliderImage key={index} src={img} alt={`CUNY ${index + 1}`} />
+            ))}
+          </Slider>
+        </SliderContainer>
+
+        {/* Right to left slider */}
+        <SliderContainer>
+          <Slider {...{ ...sliderSettings, rtl: true }}>
+            {cunyImages.map((img, index) => (
+              <SliderImage key={index} src={img} alt={`CUNY ${cunyImages.length - index}`} />
+            ))}
+          </Slider>
+        </SliderContainer>
+        
         <Flex justify="center">
           <Button
             colorScheme="blue"
